94071092a1308ad1d36280ce0918d0a66aaabdde
Norbert Harrer <nharrer@equilaris.at>
2025-04-25 13:56:43 +0200

    added geolocation service

---
+++ src/app/geolocation.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { CallbackID, Geolocation, Position } from '@capacitor/geolocation';
+import { Platform } from '@ionic/angular/standalone';
+import { BehaviorSubject, concatMap, from, of, tap } from 'rxjs';
+
+export class Coords {
+    // initial fake coords
+    public lat = 0;
+    public long = 0;
+    public accuracy = 0;
+    public timestamp = +(new Date());
+    public exist = false;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GeolocationService {
+    private readonly GEO_TIMEOUT = 30 * 1000;
+    private readonly POS_OPTS: PositionOptions;
+
+    private coordsSubject = new BehaviorSubject<Coords>(new Coords());  // new subscriptions always gets the last known coords right away
+    public coords$ = this.coordsSubject.asObservable();
+    public logs: Array<string> = [];
+
+    private callbackId: CallbackID | null = null;
+
+    constructor(
+        private platform: Platform
+    ) {
+        this.POS_OPTS = {
+            // Note: mit enableHighAccuracy = false, funktioniert es unter Android überhaupt nicht.
+            enableHighAccuracy: true
+        };
+        this.init();
+    }
+
+    public init() {
+        from(this.platform.ready()).subscribe(() => {
+            of(1).pipe(
+                tap(_ => console.log('GeolocationService', 'init', 'calling geolocation.watchPosition()')),
+                concatMap(_ => from(Geolocation.watchPosition(
+                    this.POS_OPTS,
+                    (position: Position | null, err: any) => {
+                        this.handleGeocallback(position, err);
+                    }))
+                )
+            ).subscribe((id: CallbackID) => {
+                this.callbackId = id;
+                console.log('GeolocationService', 'init', `done geolocation.watchPosition(): id=${id}`);
+            });
+        });
+    }
+
+    public updateGeoPosition(): void {
+        console.log('GeolocationService', 'updateGeoPosition', 'calling geolocation.getCurrentPosition()');
+
+        from(Geolocation.getCurrentPosition(this.POS_OPTS)).subscribe({
+            next: (position: Position) => {
+                console.log('GeolocationService', 'updateGeoPosition', 'next called.');
+                this.handleGeocallback(position, null);
+            },
+            error: (error) => {
+                console.log('GeolocationService', 'updateGeoPosition', 'error called.');
+                this.handleGeocallback(null, error);
+            }
+        });
+    }
+
+    private handleGeocallback(position: Position | null, error: any): void {
+        this.log(`GeolocationService handleGeocallbackCoords position: ${JSON.stringify(position)}, error: ${JSON.stringify(error)}`);
+
+        if (position && position.coords && position.coords.latitude) {
+            const coords = new Coords();
+            coords.lat = position.coords.latitude;
+            coords.long = position.coords.longitude;
+            coords.accuracy = position.coords.accuracy;
+            coords.timestamp = position.timestamp;
+            coords.exist = true;
+
+            // Emit the updated coordinates
+            this.coordsSubject.next(coords);
+        }
+    }
+
+    private log(msg: string): void {
+        this.logs.push(msg);
+        console.log(msg);
+    }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Coords, GeolocationService } from '../geolocation.service';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule],
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit, OnDestroy {
+  public coords: Coords | null = null;
+  public elapsed = NaN;
+  private intervalId: any;
+
+  constructor(
+    public geolocationService: GeolocationService,
+  ) {
+    this.geolocationService.coords$.subscribe((coords: Coords) => {
+      this.coords = coords;
+    });
+  }
+
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      if (this.coords) {
+        const timestamp = this.coords.timestamp;
+        const now = Date.now(); // current time in milliseconds
+        const elapsedMilliseconds = now - timestamp;
+        const elapsedSeconds = elapsedMilliseconds / 1000.0;
+        this.elapsed = elapsedSeconds;
+      }
+    }, 1000); // update every second
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
 }
