61a43158b4064b526749f93f4e5fa5e78ac8fedc
Norbert Harrer <nharrer@equilaris.at>
2025-04-25 12:41:54 +0200

    added platform ios

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.netzgewitter.ionicgeolocation',
+  appName: 'ionic-geo-location',
+  webDir: 'www'
+};
+
+export default config;
